3d3b3de69227ceca28d6bdecb7fd644996888c07
Carl Baccus <cjbaccus@gmail.com>
2019-12-06 13:56:52 -0800

    Added updates to PIP

--- PyPipInstall.py
+++ PyPipInstall.py
@@ -14,3 +14,15 @@ for line in allfiles:
 	print(text)
 	os.system("sudo pip install " + line)
 
+print("\n\nAll done with python 2 pip installs\n\n")
+print("*"*50)
+
+for line3 in allfiles:
+	text = line
+	print("installing PIP3 package")
+	print("~"*50)
+	os.system("sudo python3 -m pip install " + line)
+
+print("\n\nAll Python3 PIP packages complete\n\n")
+print("~"*50)
+
